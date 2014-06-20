53720377970a05791464fc7614e0f0bc245550ab
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-06-20 01:29:59 -0500

    drivers: usb: added USB Fast charging
    Signed-off-by: Carlos Jesús <xxx.reptar.rawrr.xxx@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/Makefile
    	drivers/usb/otg/msm_otg.c

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -54,6 +54,11 @@
 #include <mach/devices_cmdline.h>
 #include <mach/socinfo.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1354,8 +1359,20 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 	if (motg->cur_power == mA)
 		return;
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is ON - 1000mA.\n");
+	} else {
+		pr_info("USB fast charging is OFF.\n");
+	}
+
+#else
+
 	if (mA > 0)
 		mA = IDEV_ACA_CHG_MAX;
+
+#endif
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
